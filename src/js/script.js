664997a16cb0138b3a8f121fa139868cf76ac37b
Noahbrrt <noah.barret@edu.univ-fcomte.fr>
2024-12-18 14:18:50 +0100

    repository et index

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+
+  const menu = document.getElementById("menu");
+  const openMenuBtn = document.querySelector(".header__menu-btn");
+  const closeMenuBtn = document.querySelector(".menu-toggle");
+
+  openMenuBtn.addEventListener("click", () => {
+    menu.classList.add("menu--active");
+  });
+
+  closeMenuBtn.addEventListener("click", () => {
+    menu.classList.remove("menu--active");
+  });
